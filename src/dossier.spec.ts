ae7370a38329eb1f67687f65c47f3ebfc7b272e0
pengfei.dong <mail@peng-fei.name>
2022-09-28 12:19:26 +0200

    mv gsl file to ts file

--- src/dossier.spec.ts
+++ src/dossier.spec.ts
@@ -3,10 +3,6 @@ import { dsApiClient } from "./__helpers";
 describe("Dossier (unit)", () => {
   it("Has to retrieve dossier number ", async () => {
     const response = await dsApiClient.dossier(2);
-    expect(response).toEqual({
-      dossier: {
-        id: "RG9zc2llci0y",
-      },
-    });
+    expect(response.dossier.id).toBe("RG9zc2llci0y");
   });
 });
