7f1e0a20ff6db191db0dbeecc763d952da85ae61
pengfei.dong <mail@peng-fei.name>
2022-09-29 15:54:22 +0200

    add fragment dossier in procedure

--- src/graphql/getDemarche.ts
+++ src/graphql/getDemarche.ts
@@ -1,11 +1,71 @@
 import { gql } from "graphql-request";
 
 export default gql`
-    query getDemarche($demarcheNumber: Int!) {
+    query getDemarche(
+        $demarcheNumber: Int!
+        $state: DossierState
+        $order: Order
+        $after: String
+    ) {
         demarche(number: $demarcheNumber) {
+            id
             number
             title
-            description
+            groupeInstructeurs {
+                id
+                number
+                label
+                instructeurs {
+                    id
+                    email
+                }
+            }
+            dossiers(state: $state, order: $order, after: $after) {
+                nodes {
+                    ...DossierFragment
+                }
+            }
+        }
+    }
+    
+    fragment DossierFragment on Dossier {
+        id
+        number
+        archived
+        state
+        dateDerniereModification
+        dateDepot
+        datePassageEnConstruction
+        datePassageEnInstruction
+        dateTraitement
+        motivation
+        pdf {
+            url
+        }
+        instructeurs {
+            email
+        }
+        usager {
+            email
+        }
+        groupeInstructeur {
+            id
+            number
+            label
+        }
+        traitements {
+            state
+            emailAgentTraitant
+            dateTraitement
+            motivation
+        }
+        demandeur {
+            ... on PersonnePhysique {
+                civilite
+                nom
+                prenom
+                dateDeNaissance
+            }
         }
     }
 `;
