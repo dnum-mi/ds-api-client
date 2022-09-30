d090f741c36e963d065848a53a01e0f7dabd49e4
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2022-09-30 16:06:17 +0200

    Disable linter auto-fix

--- src/graphql/getGroupInstructeur.ts
+++ src/graphql/getGroupInstructeur.ts
@@ -1,25 +1,25 @@
 import { gql } from "graphql-request";
 
 export default gql`
-    query getGroupeInstructeur(
-        $groupeInstructeurNumber: Int!
-        $state: DossierState
-        $order: Order
-        $after: String
-    ) {
-        groupeInstructeur(number: $groupeInstructeurNumber) {
-            id
-            number
-            label
-            instructeurs {
-                id
-                email
-            }
-            dossiers(state: $state, order: $order, after: $after) {
-                nodes {
-                    ...DossierFragment
-                }
-            }
+  query getGroupeInstructeur(
+    $groupeInstructeurNumber: Int!
+    $state: DossierState
+    $order: Order
+    $after: String
+  ) {
+    groupeInstructeur(number: $groupeInstructeurNumber) {
+      id
+      number
+      label
+      instructeurs {
+        id
+        email
+      }
+      dossiers(state: $state, order: $order, after: $after) {
+        nodes {
+          ...DossierFragment
         }
+      }
     }
+  }
 `;
