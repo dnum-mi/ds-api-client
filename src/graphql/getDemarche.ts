30a60c51ce9e4096bb5b3be9262fc54aa519ae54
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2022-09-30 16:06:17 +0200

    Disable linter auto-fix

--- src/graphql/getDemarche.ts
+++ src/graphql/getDemarche.ts
@@ -1,71 +1,71 @@
 import { gql } from "graphql-request";
 
 export default gql`
-    query getDemarche(
-        $demarcheNumber: Int!
-        $state: DossierState
-        $order: Order
-        $after: String
-    ) {
-        demarche(number: $demarcheNumber) {
-            id
-            number
-            title
-            groupeInstructeurs {
-                id
-                number
-                label
-                instructeurs {
-                    id
-                    email
-                }
-            }
-            dossiers(state: $state, order: $order, after: $after) {
-                nodes {
-                    ...DossierFragment
-                }
-            }
-        }
-    }
-    
-    fragment DossierFragment on Dossier {
+  query getDemarche(
+    $demarcheNumber: Int!
+    $state: DossierState
+    $order: Order
+    $after: String
+  ) {
+    demarche(number: $demarcheNumber) {
+      id
+      number
+      title
+      groupeInstructeurs {
         id
         number
-        archived
-        state
-        dateDerniereModification
-        dateDepot
-        datePassageEnConstruction
-        datePassageEnInstruction
-        dateTraitement
-        motivation
-        pdf {
-            url
-        }
+        label
         instructeurs {
-            email
+          id
+          email
         }
-        usager {
-            email
+      }
+      dossiers(state: $state, order: $order, after: $after) {
+        nodes {
+          ...DossierFragment
         }
-        groupeInstructeur {
-            id
-            number
-            label
-        }
-        traitements {
-            state
-            emailAgentTraitant
-            dateTraitement
-            motivation
-        }
-        demandeur {
-            ... on PersonnePhysique {
-                civilite
-                nom
-                prenom
-                dateDeNaissance
-            }
-        }
+      }
     }
+  }
+
+  fragment DossierFragment on Dossier {
+    id
+    number
+    archived
+    state
+    dateDerniereModification
+    dateDepot
+    datePassageEnConstruction
+    datePassageEnInstruction
+    dateTraitement
+    motivation
+    pdf {
+      url
+    }
+    instructeurs {
+      email
+    }
+    usager {
+      email
+    }
+    groupeInstructeur {
+      id
+      number
+      label
+    }
+    traitements {
+      state
+      emailAgentTraitant
+      dateTraitement
+      motivation
+    }
+    demandeur {
+      ... on PersonnePhysique {
+        civilite
+        nom
+        prenom
+        dateDeNaissance
+      }
+    }
+  }
 `;
--- src/graphql/getDossier.ts
+++ src/graphql/getDossier.ts
@@ -1,299 +1,299 @@
 import { gql } from "graphql-request";
 
 export default gql`
-    query getDossier($dossierNumber: Int!) {
-        dossier(number: $dossierNumber) {
-            ...DossierFragment
-            demarche {
-                ...DemarcheDescriptorFragment
-            }
-        }
+  query getDossier($dossierNumber: Int!) {
+    dossier(number: $dossierNumber) {
+      ...DossierFragment
+      demarche {
+        ...DemarcheDescriptorFragment
+      }
     }
-    
-    fragment DossierFragment on Dossier {
+  }
+
+  fragment DossierFragment on Dossier {
+    id
+    number
+    archived
+    state
+    dateDerniereModification
+    dateDepot
+    datePassageEnConstruction
+    datePassageEnInstruction
+    dateTraitement
+    motivation
+    motivationAttachment {
+      ...FileFragment
+    }
+    attestation {
+      ...FileFragment
+    }
+    pdf {
+      url
+    }
+    instructeurs {
+      email
+    }
+    usager {
+      email
+    }
+    groupeInstructeur {
+      id
+      number
+      label
+    }
+    revision {
+      ...RevisionFragment
+    }
+    traitements {
+      state
+      emailAgentTraitant
+      dateTraitement
+      motivation
+    }
+    champs {
+      ...ChampFragment
+      ...RootChampFragment
+    }
+    annotations {
+      ...ChampFragment
+      ...RootChampFragment
+    }
+    avis {
+      ...AvisFragment
+    }
+    messages {
+      ...MessageFragment
+    }
+    demandeur {
+      ... on PersonnePhysique {
+        civilite
+        nom
+        prenom
+        dateDeNaissance
+      }
+      ...PersonneMoraleFragment
+    }
+  }
+
+  fragment DemarcheDescriptorFragment on DemarcheDescriptor {
+    id
+    number
+    title
+    description
+    state
+    declarative
+    dateCreation
+    datePublication
+    dateDerniereModification
+    dateDepublication
+    dateFermeture
+  }
+
+  fragment RevisionFragment on Revision {
+    id
+    champDescriptors {
+      ...ChampDescriptorFragment
+      champDescriptors {
+        ...ChampDescriptorFragment
+      }
+    }
+    annotationDescriptors {
+      ...ChampDescriptorFragment
+      champDescriptors {
+        ...ChampDescriptorFragment
+      }
+    }
+  }
+
+  fragment ChampDescriptorFragment on ChampDescriptor {
+    id
+    type
+    label
+    description
+    required
+    options
+  }
+
+  fragment AvisFragment on Avis {
+    id
+    question
+    reponse
+    dateQuestion
+    dateReponse
+    claimant {
+      email
+    }
+    expert {
+      email
+    }
+    attachment {
+      ...FileFragment
+    }
+  }
+
+  fragment MessageFragment on Message {
+    id
+    email
+    body
+    createdAt
+    attachment {
+      ...FileFragment
+    }
+  }
+
+  fragment GeoAreaFragment on GeoArea {
+    id
+    source
+    description
+    geometry {
+      type
+      coordinates
+    }
+    ... on ParcelleCadastrale {
+      commune
+      numero
+      section
+      prefixe
+      surface
+    }
+  }
+
+  fragment RootChampFragment on Champ {
+    ... on RepetitionChamp {
+      champs {
+        ...ChampFragment
+      }
+    }
+    ... on SiretChamp {
+      etablissement {
+        ...PersonneMoraleFragment
+      }
+    }
+    ... on CarteChamp {
+      geoAreas {
+        ...GeoAreaFragment
+      }
+    }
+    ... on DossierLinkChamp {
+      dossier {
         id
-        number
-        archived
         state
-        dateDerniereModification
-        dateDepot
-        datePassageEnConstruction
-        datePassageEnInstruction
-        dateTraitement
-        motivation
-        motivationAttachment {
-            ...FileFragment
-        }
-        attestation {
-            ...FileFragment
-        }
-        pdf {
-            url
-        }
-        instructeurs {
-            email
-        }
         usager {
-            email
+          email
         }
-        groupeInstructeur {
-            id
-            number
-            label
-        }
-        revision {
-            ...RevisionFragment
-        }
-        traitements {
-            state
-            emailAgentTraitant
-            dateTraitement
-            motivation
-        }
-        champs {
-            ...ChampFragment
-            ...RootChampFragment
-        }
-        annotations {
-            ...ChampFragment
-            ...RootChampFragment
-        }
-        avis {
-            ...AvisFragment
-        }
-        messages {
-            ...MessageFragment
-        }
-        demandeur {
-            ... on PersonnePhysique {
-                civilite
-                nom
-                prenom
-                dateDeNaissance
-            }
-            ...PersonneMoraleFragment
-        }
+      }
     }
+  }
 
-    fragment DemarcheDescriptorFragment on DemarcheDescriptor {
-        id
-        number
-        title
-        description
-        state
-        declarative
-        dateCreation
-        datePublication
-        dateDerniereModification
-        dateDepublication
-        dateFermeture
+  fragment ChampFragment on Champ {
+    id
+    label
+    stringValue
+    ... on DateChamp {
+      date
     }
+    ... on DatetimeChamp {
+      datetime
+    }
+    ... on CheckboxChamp {
+      checked: value
+    }
+    ... on DecimalNumberChamp {
+      decimalNumber: value
+    }
+    ... on IntegerNumberChamp {
+      integerNumber: value
+    }
+    ... on CiviliteChamp {
+      civilite: value
+    }
+    ... on LinkedDropDownListChamp {
+      primaryValue
+      secondaryValue
+    }
+    ... on MultipleDropDownListChamp {
+      values
+    }
+    ... on PieceJustificativeChamp {
+      file {
+        ...FileFragment
+      }
+    }
+    ... on AddressChamp {
+      address {
+        ...AddressFragment
+      }
+    }
+    ... on CommuneChamp {
+      commune {
+        name
+        code
+      }
+      departement {
+        name
+        code
+      }
+    }
+  }
 
-    fragment RevisionFragment on Revision {
-        id
-        champDescriptors {
-            ...ChampDescriptorFragment
-            champDescriptors {
-                ...ChampDescriptorFragment
-            }
-        }
-        annotationDescriptors {
-            ...ChampDescriptorFragment
-            champDescriptors {
-                ...ChampDescriptorFragment
-            }
-        }
+  fragment PersonneMoraleFragment on PersonneMorale {
+    siret
+    siegeSocial
+    naf
+    libelleNaf
+    address {
+      ...AddressFragment
     }
+    entreprise {
+      siren
+      capitalSocial
+      numeroTvaIntracommunautaire
+      formeJuridique
+      formeJuridiqueCode
+      nomCommercial
+      raisonSociale
+      siretSiegeSocial
+      codeEffectifEntreprise
+      dateCreation
+      nom
+      prenom
+      attestationFiscaleAttachment {
+        ...FileFragment
+      }
+      attestationSocialeAttachment {
+        ...FileFragment
+      }
+    }
+    association {
+      rna
+      titre
+      objet
+      dateCreation
+      dateDeclaration
+      datePublication
+    }
+  }
 
-    fragment ChampDescriptorFragment on ChampDescriptor {
-        id
-        type
-        label
-        description
-        required
-        options
-    }
+  fragment FileFragment on File {
+    filename
+    contentType
+    checksum
+    byteSizeBigInt
+    url
+  }
 
-    fragment AvisFragment on Avis {
-        id
-        question
-        reponse
-        dateQuestion
-        dateReponse
-        claimant {
-            email
-        }
-        expert {
-            email
-        }
-        attachment {
-            ...FileFragment
-        }
-    }
-
-    fragment MessageFragment on Message {
-        id
-        email
-        body
-        createdAt
-        attachment {
-            ...FileFragment
-        }
-    }
-
-    fragment GeoAreaFragment on GeoArea {
-        id
-        source
-        description
-        geometry {
-            type
-            coordinates
-        }
-        ... on ParcelleCadastrale {
-            commune
-            numero
-            section
-            prefixe
-            surface
-        }
-    }
-
-    fragment RootChampFragment on Champ {
-        ... on RepetitionChamp {
-            champs {
-                ...ChampFragment
-            }
-        }
-        ... on SiretChamp {
-            etablissement {
-                ...PersonneMoraleFragment
-            }
-        }
-        ... on CarteChamp {
-            geoAreas {
-                ...GeoAreaFragment
-            }
-        }
-        ... on DossierLinkChamp {
-            dossier {
-                id
-                state
-                usager {
-                    email
-                }
-            }
-        }
-    }
-
-    fragment ChampFragment on Champ {
-        id
-        label
-        stringValue
-        ... on DateChamp {
-            date
-        }
-        ... on DatetimeChamp {
-            datetime
-        }
-        ... on CheckboxChamp {
-            checked: value
-        }
-        ... on DecimalNumberChamp {
-            decimalNumber: value
-        }
-        ... on IntegerNumberChamp {
-            integerNumber: value
-        }
-        ... on CiviliteChamp {
-            civilite: value
-        }
-        ... on LinkedDropDownListChamp {
-            primaryValue
-            secondaryValue
-        }
-        ... on MultipleDropDownListChamp {
-            values
-        }
-        ... on PieceJustificativeChamp {
-            file {
-                ...FileFragment
-            }
-        }
-        ... on AddressChamp {
-            address {
-                ...AddressFragment
-            }
-        }
-        ... on CommuneChamp {
-            commune {
-                name
-                code
-            }
-            departement {
-                name
-                code
-            }
-        }
-    }
-
-    fragment PersonneMoraleFragment on PersonneMorale {
-        siret
-        siegeSocial
-        naf
-        libelleNaf
-        address {
-            ...AddressFragment
-        }
-        entreprise {
-            siren
-            capitalSocial
-            numeroTvaIntracommunautaire
-            formeJuridique
-            formeJuridiqueCode
-            nomCommercial
-            raisonSociale
-            siretSiegeSocial
-            codeEffectifEntreprise
-            dateCreation
-            nom
-            prenom
-            attestationFiscaleAttachment {
-                ...FileFragment
-            }
-            attestationSocialeAttachment {
-                ...FileFragment
-            }
-        }
-        association {
-            rna
-            titre
-            objet
-            dateCreation
-            dateDeclaration
-            datePublication
-        }
-    }
-
-    fragment FileFragment on File {
-        filename
-        contentType
-        checksum
-        byteSizeBigInt
-        url
-    }
-
-    fragment AddressFragment on Address {
-        label
-        type
-        streetAddress
-        streetNumber
-        streetName
-        postalCode
-        cityName
-        cityCode
-        departmentName
-        departmentCode
-        regionName
-        regionCode
-    }
+  fragment AddressFragment on Address {
+    label
+    type
+    streetAddress
+    streetNumber
+    streetName
+    postalCode
+    cityName
+    cityCode
+    departmentName
+    departmentCode
+    regionName
+    regionCode
+  }
 `;
