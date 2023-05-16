9720c66597daf4d9803a6cf85fa2f70697302120
Robin <pulsarr@Pulsarr-MacbookPro-M2.local>
2023-05-16 18:21:11 +0200

    better export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { DsApiClient } from "./ds-api-client";
 import { DsApiError } from "./ds-api-error";
 
 export { DsApiClient, DsApiError };
+
+export * from "./@types/graphql";
 export * from "./@types/types";
