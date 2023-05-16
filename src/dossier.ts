30af1cd12d7e974b9fe137f815ca2babc06bf1f6
Robin <pulsarr@Pulsarr-MacbookPro-M2.local>
2023-05-16 16:36:37 +0200

    New basic error management, regrouping request behavior at the same place. Exporting new information. Typing result of functions

--- src/dossier.ts
+++ src/dossier.ts
@@ -2,15 +2,14 @@ import { GraphQLClient } from "graphql-request";
 import { Dossier } from "./@types/types";
 
 import query from "./graphql/getDossier";
+import { graphQlRequest } from "./common";
 
-export const getDossier = async (client: GraphQLClient, idDossier: number) => {
-  try {
-    return await client.request<{ dossier: Partial<Dossier> }>(
-      query,
-      { dossierNumber: idDossier },
-      { "Content-Type": "application/json" },
-    );
-  } catch (error) {
-    console.error(JSON.stringify(error.stack, undefined, 2));
-  }
+type getDossierType = { dossier: Partial<Dossier> };
+export const getDossier = async (
+  client: GraphQLClient,
+  idDossier: number,
+): Promise<getDossierType> => {
+  return graphQlRequest<getDossierType>(client, query, {
+    dossierNumber: idDossier,
+  });
 };
--- src/groupeInstructeur.ts
+++ src/groupeInstructeur.ts
@@ -1,21 +1,17 @@
 import { GraphQLClient } from "graphql-request";
 import { GroupeInstructeur } from "./@types/types";
-
 import query from "./graphql/getGroupInstructeur";
+import { graphQlRequest } from "./common";
+
+type getGroupInstructeurType = {
+  groupeInstructeur: Partial<GroupeInstructeur>;
+};
 
 export const getGroupInstructeur = async (
   client: GraphQLClient,
   idGroupeInstructeur: number,
-) => {
-  try {
-    return await client.request<{
-      groupeInstructeur: Partial<GroupeInstructeur>;
-    }>(
-      query,
-      { groupeInstructeurNumber: idGroupeInstructeur },
-      { "Content-Type": "application/json" },
-    );
-  } catch (error) {
-    console.error(JSON.stringify(error.stack, undefined, 2));
-  }
+): Promise<getGroupInstructeurType> => {
+  return graphQlRequest<getGroupInstructeurType>(client, query, {
+    groupeInstructeurNumber: idGroupeInstructeur,
+  });
 };
