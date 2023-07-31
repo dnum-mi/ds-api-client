1740de002a77cc2010bebf1a71b2a5729ab940e6
Pulsarr <dev@pulsarr.fr>
2023-07-31 15:12:46 +0200

    feat: custom champ and repetition champ (#60)
    * Updating version of schemaGQL to have rows on RepetionChamps
    
    * getDossierCustom return champs with champs descriptor associated
    
    * separating custom types from generated types
    
    * building another function to have CustomChamp on list of dossier in demarche.
    
    * adding updatedSince variable
    
    * deleting unit test that are not unit

--- src/__helpers.ts
+++
@@ -1,8 +0,0 @@
-import { DsApiClient } from "./index";
-
-// TODO: test should not really contact DsApiClient. It should mock it.
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
-const { API_URL, API_TOKEN } = process.env;
-const dsApiClient = new DsApiClient(API_URL, API_TOKEN);
-export { dsApiClient };
