17ebeeb1df7dc75815e1fa3bdd7718870c18dee2
DIDIERRobin <DIDIERRobin@users.noreply.github.com>
2023-08-30 13:31:16 +0000

    Update dev-release/test-1 to output generated at 427a305

--- dist/dossier/dossier-custom-champ.js
+++ dist/dossier/dossier-custom-champ.js
@@ -3,6 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDossierWithCustomChamp = exports.mergeChampAndChampDescriptor = void 0;
 const common_1 = require("../common");
 const getDossier_1 = require("../graphql/getDossier");
+const extractSmallId = (base64String) => {
+    const decodedString = Buffer.from(base64String, "base64").toString("utf8");
+    const pipeIndex = decodedString.indexOf("|");
+    if (pipeIndex !== -1) {
+        return Buffer.from(decodedString.substring(0, pipeIndex)).toString("base64");
+    }
+    return base64String;
+};
 const mergeChampAndChampDescriptor = (dossier) => {
     if (!dossier.revision?.champDescriptors) {
         throw new Error("Cannot map champs descriptor without revision in dossier.");
@@ -20,18 +28,16 @@ const mergeChampAndChampDescriptor = (dossier) => {
         }
         return [[descriptor.id, descriptor]];
     })
-        .flat());
+        .flat(1));
     dossier.champs = dossier.champs.map((champ) => {
         champ.champDescriptor = _hashDescriptor[champ.id];
         if (champ.__typename === "RepetitionChamp") {
-            champ.rows.map((row) => ({
-                champs: row.champs.map((subChamp) => {
-                    const smallId = Buffer.from(Buffer.from(subChamp.id, "base64")
-                        .toString("binary")
-                        .substring(0, 10), "binary").toString("base64");
-                    subChamp.champDescriptor = _hashDescriptor[smallId];
-                }),
-            }));
+            champ.rows.forEach((row) => {
+                row.champs.forEach((subChamp) => {
+                    subChamp.champDescriptor =
+                        _hashDescriptor[extractSmallId(subChamp.id)];
+                });
+            });
         }
         return champ;
     });
