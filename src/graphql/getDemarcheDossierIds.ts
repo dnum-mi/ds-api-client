af55d95c1aedff21e44d72e61be040462107dc25
baudointran <baudointran@users.noreply.github.com>
2024-02-07 10:20:29 +0100

    fix: get revsion of demarches

--- src/graphql/getDemarcheDossierIds.ts
+++ src/graphql/getDemarcheDossierIds.ts
@@ -12,6 +12,40 @@ export default gql`
       id
       number
       title
+
+      id
+      number
+      title
+      description
+      state
+
+      dateCreation
+      dateDepublication
+      dateDerniereModification
+      dateFermeture
+      datePublication
+      declarative
+
+      draftRevision {
+        ...RevisionFragment
+      }
+      publishedRevision {
+        ...RevisionFragment
+      }
+      revisions {
+        ...RevisionFragment
+      }
+
+      groupeInstructeurs {
+        id
+        number
+        label
+        instructeurs {
+          id
+          email
+        }
+      }
+
       dossiers(
         state: $state
         order: $order
@@ -23,6 +57,14 @@ export default gql`
           number
           state
         }
+      }
+
+      service {
+        id
+        nom
+        organisme
+        siret
+        typeOrganisme
       }
     }
   }
