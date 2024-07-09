c89b7f4f51ba2cb42e6a9d2424da268ffcc03acc
baudointran <baudointran@users.noreply.github.com>
2024-07-09 11:20:24 +0200

    fix: type DossierWithCustomChamp for annotation

--- src/dossier/custom-champ.type.ts
+++ src/dossier/custom-champ.type.ts
@@ -3,6 +3,8 @@ import { Champ, ChampDescriptor, Dossier } from "../@types/types";
 export type CustomChamp = Champ & {
   champDescriptor: ChampDescriptor;
 };
+
 export type DossierWithCustomChamp = Dossier & {
   champs: Array<CustomChamp>;
+  annotations: Array<CustomChamp>;
 };
